82733dcf96822844dcb35fc1481201651e46ccd3
Paweł Uchański <uchanski.pawel@gmail.com>
2022-11-20 13:44:38 +0100

    add 0auth and time endpoint

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import sympy
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, status
 import cv2
+import datetime
 import numpy as np
+from typing import Union
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 from matplotlib import pyplot as plt
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
 
@@ -10,26 +31,86 @@ app = FastAPI()
 def print_hi(name):
     print(f'Hi, {name}')
 
-# done
 @app.get("/prime/{number}", status_code=200)
 async def read_foods(number: int):
     return sympy.isprime(number)
 
 
 @app.post("/picture/invert/", status_code=201)
-def upload(file: UploadFile):
-    try:
-        image = file.file.read()
-        npAr = np.frombuffer(image, np.uint8)
-        imagev2 = cv2.imread(npAr, cv2.IMREAD_COLOR)
-        invertedImg = cv2.bitwise_not(imagev2)
-        return invertedImg;
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    finally:
-        file.file.close()
+def upload(file: UploadFile = File(...)):
+    image = file.read()
+    npAr = np.frombuffer(image, np.uint8)
+    imagev2 = cv2.imdecode(npAr, cv2.IMREAD_COLOR)
+    invertedImg = cv2.bitwise_not(imagev2)
+    return invertedImg;
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+# OAuth
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/time")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return datetime.datetime.now()
+
 
 
 if __name__ == '__main__':
